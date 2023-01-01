0dcf155e8d5bce128fe196bb1d576ade79aa190c
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-01-01 18:29:15 +0530

    Update Project

--- takehomepractise/src/session2-Takehome/DogPics.js
+++ takehomepractise/src/session2-Takehome/DogPics.js
@@ -3,3 +3,15 @@ import axios from "axios";
 const axios = require("axios");
 
 
+class DogPics extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {};
+    }
+    render() {
+      return "doGG";
+    }
+  }
+  
+  export default DogPics;
+  
