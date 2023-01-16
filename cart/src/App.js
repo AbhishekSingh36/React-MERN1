f935527aacd2632a20bad09541d5299a8a3db59a
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-01-16 11:03:25 +0530

    Updating

--- cart/src/App.js
+++ cart/src/App.js
@@ -1,8 +1,12 @@
+import React from "react";
+import CartItem from "./CartItem";
+//import "./app.css"
 
 function App() {
   return (
     <div className="App">
       <h1>Cart</h1>
+      <CartItem />
     </div>
   );
 }
