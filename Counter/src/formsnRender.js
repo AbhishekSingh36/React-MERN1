46f3fca19b5516b08861f71183621e1e177f7697
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-01-18 23:44:59 +0530

    All rendering cycle

--- Counter/src/formsnRender.js
+++ Counter/src/formsnRender.js
@@ -10,4 +10,40 @@ class Form extends React.Component {
       nameStateVariable: ""
     };
   }
+
+  componentDidMount() {
+    console.log("componentDidMount Is Called");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate Is Called");
+
+    console.log("Previous State Variable: ", prevState.nameStateVariable);
+    console.log("Current State Variable: ", this.state.nameStateVariable);
+  }
+
+  updateTextField = (event) => {
+    this.setState({ nameStateVariable: event.target.value });
+  };
+
+  render() {
+    console.log("Render Is Called");
+
+    // console.log("props from Render", this.props.testValue);
+
+    return (
+      <form>
+        <label>TextField:</label>
+        <input
+          type="text"
+          name="TextField"
+          value={this.state.nameStateVariable}
+          onChange={(event) => this.updateTextField(event)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
+
+export default Form;
