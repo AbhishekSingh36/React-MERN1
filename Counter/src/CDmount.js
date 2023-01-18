55c183884cb964e56239001074c5ff7bcbcc8b1d
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-01-18 23:43:13 +0530

    CDmount 3 lifecycle methods

--- Counter/src/CDmount.js
+++ Counter/src/CDmount.js
@@ -6,4 +6,20 @@ class CDMountActivity extends Component {
     super(props);
     this.state = { email: "" };
   }
+
+  componentDidMount() {
+    this.getEmail();
+  }
+  getEmail = async () => {
+    let res = await axios.get("https://reqres.in/api/users/1");
+    let email = res.data.data.email;
+    console.log(email);
+
+    this.setState({ email: email });
+  };
+  render() {
+    return <div>{this.state.email}</div>;
+  }
 }
+
+export default CDMountActivity;
