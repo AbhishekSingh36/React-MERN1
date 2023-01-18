73e69e140466264f0c1c518a0c89037c4bc4c77b
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-01-18 23:42:41 +0530

    CDmount 3 lifecycle methods

---
+++ Counter/src/CDmount.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import React, { Component } from "react";
+
+class CDMountActivity extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: "" };
+  }
+}
