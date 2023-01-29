58bfb15cc354ac93610127321e0ba8546bb3e8a1
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-01-29 22:04:09 +0530

    Update

--- Counter/src/index.js
+++ Counter/src/index.js
@@ -1,52 +1,11 @@
 import ReactDOM from "react-dom";
 import React, { Component } from "react";
-import "../styles.css";
+import './index.css'
+import App from './App'
 
-class Counter extends React.Component {
-  // class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-  }
-
-  increment = () => {
-    // Increase the value of Count by 1
-    this.setState((currState) => {
-      return { count: currState.count + this.props.diff };
-    });
-  };
-
-  decrement = () => {
-    // Decrease the value fo Count by 1
-    this.setState((currState) => {
-      return { count: currState.count - this.props.diff };
-    });
-  };
-
-  render() {
-    // let variable = 2;
-    return (
-      <div>
-        <h1 className="counter-display">{this.state.count}</h1>
-        <button onClick={this.increment}>+{this.props.diff}</button>
-        <button onClick={this.decrement}>-{this.props.diff}</button>
-        {/* {variable} */}
-      </div>
-    );
-  }
-}
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Counter diff={1} />
-        <Counter diff={10} />
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
