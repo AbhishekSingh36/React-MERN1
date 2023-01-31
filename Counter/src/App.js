733973e4f3301eb36c38166bd509c3728f4c61ce
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-01-31 22:23:47 +0530

    Update

--- Counter/src/App.js
+++ Counter/src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import Header from './components/Header';
 import Display from './components/Display';
 import Button from '../src/components/Button';
+import { render } from "react-dom";
+
+
 
 class App extends React.Component{
     render(){
         return <div>
             <Header/>
             <Display/>
+            <Button/>
             <Button/>
         </div>
     }
