8360a4c20c234cc9d660a088437e1cacc5fa19a8
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-02-02 23:57:35 +0530

    Update

--- Counter/src/components/Button.js
+++ Counter/src/components/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class Button extends React.Component {
     render(){
-        return <div>Button Component</div>
+        return <div>Button Component 1</div>
     }
 }
 
