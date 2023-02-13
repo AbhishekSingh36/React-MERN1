051e5d6333a49dbab4206487531c2e7834717c7f
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-02-13 21:52:28 +0530

    Update

--- Counter/src/App.js
+++ Counter/src/App.js
@@ -7,48 +7,52 @@ import { render } from "react-dom";
 
 
 
-class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            counter: 0
-        }
-    }
-    handleIncrement = () => {
-        this.setState((prevState) => ({
-            counter: prevState.counter + 1
-        }))
-    }
-    handleDecrement = () => {
-        this.setState((prevState) => ({
-            counter: prevState.counter - 1
-        }))
-    }
-    render() {
-        // Explicit conditional rendring
-        if (this.state.counter === 10) {
-            return <div>Congrats You have reached 10</div>
-        }
-        return <div>
-            {/* Props Example */}
-            {/* <Header headerText= 'Counter Application' />
-            <Header headerText= 'Counter Application2 Props i.e Custom attributes' /> 
-            <Button handleButtonFunction={()=> console.log('plus Button')} buttonText = '+'/>
-            <Display displayText = 'Display'/>  
-            <Button handleButtonFunction={()=> console.log('plus Button')} buttonText = '-'/> */}
+// class App extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             counter: 0
+//         }
+//     }
+//     handleIncrement = () => {
+//         this.setState((prevState) => ({
+//             counter: prevState.counter + 1
+//         }))
+//     }
+//     handleDecrement = () => {
+//         this.setState((prevState) => ({
+//             counter: prevState.counter - 1
+//         }))
+//     }
+//     render() {
+//         // Explicit conditional rendring
+//         if (this.state.counter === 10) {
+//             return <div>Congrats You have reached 10</div>
+//         }
+//         return <div>
+//             {/* Props Example */}
+//             {/* <Header headerText= 'Counter Application' />
+//             <Header headerText= 'Counter Application2 Props i.e Custom attributes' /> 
+//             <Button handleButtonFunction={()=> console.log('plus Button')} buttonText = '+'/>
+//             <Display displayText = 'Display'/>  
+//             <Button handleButtonFunction={()=> console.log('plus Button')} buttonText = '-'/> */}
 
-            {/* Inline conditional rendring */}
-            <Text displayText="Counter Application" />
-            {!this.state.counter !== 10 ?
-                <div>
-                    <Button btnClickHandler={this.handleDecrement} btnText="-" />
-                    <Text displayText={this.state.counter} />
-                    <Button btnClickHandler={this.handleIncrement} btnText="+" />
-                </div> : <div>Congrats - Your have reached 10</div>
-            }
+//             {/* Inline conditional rendring */}
+//             <Text displayText="Counter Application" />
+//             {!this.state.counter !== 10 ?
+//                 <div>
+//                     <Button btnClickHandler={this.handleDecrement} btnText="-" />
+//                     <Text displayText={this.state.counter} />
+//                     <Button btnClickHandler={this.handleIncrement} btnText="+" />
+//                 </div> : <div>Congrats - Your have reached 10</div>
+//             }
 
-        </div>
-    }
+//         </div>
+//     }
+// }
+
+function App(){
+    return
 }
 
 export default App
