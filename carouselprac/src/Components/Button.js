8b16340b40eb4bda05d972b4f3f0b6855a9d4949
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-02-14 21:17:42 +0530

    Update

--- carouselprac/src/Components/Button.js
+++ carouselprac/src/Components/Button.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Button = (props) => {
     const {btnText="" , btnHandler =() => {}, btnBackground="black", isDisabled=false} = props
