60574f6919d5904134e14a6608c97a4434326743
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-02-14 15:17:51 +0530

    Button update

--- carouselprac/src/Components/Button.js
+++ carouselprac/src/Components/Button.js
@@ -2,5 +2,5 @@ import React from "react";
 
 const Button = (props) => {
     const {btnText="" , btnHandler =() => {}, btnBackground="black"} = props
-    return <button onClick={} >{btnText}</button>
+    return <button onClick={btnHandler} >{btnText}</button>
 }
