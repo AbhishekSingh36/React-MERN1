01e20a3177e8df93b165632a981ee19689cb60fe
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-02-28 23:07:35 +0530

    Update

--- video/src/VideoCard.js
+++ video/src/VideoCard.js
@@ -14,7 +14,31 @@ const VideoCard = (props) => {
     title = "",
     releaseDate = ""
   } = props;
-  return
+  return  (
+    <Grid item>
+      <Card sx={{ maxWidth: 345, minWidth: 345, minHeight: 200 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={previewImage}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              {genre}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div">
+              {title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {releaseDate}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Grid>
+  );
 };
 
 export default VideoCard;
