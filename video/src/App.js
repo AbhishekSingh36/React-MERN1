035602fac2d31dc361c97e59d2c9fd00548c4a32
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-02-28 23:05:04 +0530

    Update

--- video/src/App.js
+++ video/src/App.js
@@ -1,23 +1,9 @@
-import * as React from "react";
-import { useState, useEffect } from "react";
-import "./styles.css";
-import axios from "axios";
-import Button from "@mui/material/Button";
+import HomePage from "./HomePage";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const handleIncrease = () => {
-    setCount(count + 1);
-  };
-  useEffect(() => {
-    console.log("Hello");
-  });
-
   return (
-    <div className="App">
-      {count}
-      <button onClick={handleIncrease}>Increase</button>
-      
+    <div>
+      <HomePage />
     </div>
   );
 }
