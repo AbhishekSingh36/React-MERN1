0da7caf8da57ef1dfaec48bb76d978e1851b9f2b
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-02-28 23:07:22 +0530

    Update

---
+++ video/src/VideoCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+const VideoCard = (props) => {
+  const {
+    id = "",
+    previewImage = "",
+    genre = "",
+    title = "",
+    releaseDate = ""
+  } = props;
+  return
+};
+
+export default VideoCard;
