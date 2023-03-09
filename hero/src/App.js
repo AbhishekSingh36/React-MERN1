97c2ba45c5721c95c29476bbc45ade7a135e8fc2
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-03-09 22:16:12 +0530

    Update Project

--- hero/src/App.js
+++ hero/src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Built a Hero</h1>
+      <label>Name</label>
+      <input type='text' />
     </div>
   );
 }
