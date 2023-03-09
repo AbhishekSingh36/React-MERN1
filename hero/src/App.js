006f12907f4cab9868847f36f486e1b96f75e126
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-03-09 22:17:54 +0530

    Update Project

--- hero/src/App.js
+++ hero/src/App.js
@@ -5,7 +5,13 @@ function App() {
   return (
     <div className="App">
       <h1>Built a Hero</h1>
-      <label>Name</label>
+      <label>Name: </label>
+      <input type='text' />
+      <label>Age: </label>
+      <input type='Number' />
+      <label>Height: </label>
+      <input type='text' />
+      <label>SuperPower: </label>
       <input type='text' />
     </div>
   );
