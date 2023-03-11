392c55b4a8a976f19667f35427df4b08cce86ca2
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-03-11 17:59:10 +0530

    Update Project

--- takehomepractise/src/session1-takeHome/DashBord.js
+++ takehomepractise/src/session1-takeHome/DashBord.js
@@ -1,9 +1,29 @@
+import "./styles.css";
+
 export default function App() {
-    return (
-      <div className="App">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <form>
-          <label>
+  return (
+    <div className="App">
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <form>
+        <label>
+          Name:
+          <input type="text" className="name" placeHolder="Name" />
+        </label>
+        <br />
+        <br />
+        <label>
+          Email
+          <input type="text" className="className" placeHolder="Email" />
+        </label>
+      </form>
+      <img
+        src="https://reactjs.org/logo-og.png"
+        alt="React Image"
+        className="rImage"
+      />
+    </div>
+  );
+}
