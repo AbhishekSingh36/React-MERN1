9e5944f783790d48b5414ba9679194ee2460c669
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-03-17 22:33:42 +0530

    Update Project

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -7,7 +7,8 @@ function App() {
   <div className='App'>
     <h1>ToDo List</h1>
     <div>
-      
+      <input type="text" placeholder='Task'/>
+      <button>Add Task</button>
     </div>
 
   </div>
