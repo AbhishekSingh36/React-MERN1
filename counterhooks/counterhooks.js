ea2bc2ae59b8bda5b72684cb1e3661a9edfb7075
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-03-26 07:17:32 +0530

    Update Projct

--- counterhooks/counterhooks.js
+++ counterhooks/counterhooks.js
@@ -14,8 +14,8 @@ const CounterHooks = ({diff}) => {
     return (
         <div>
             <h1 className="counter-display">{count}</h1>
-            <button className={increment}>+8{diff}</button>
-            <button className={decrement}>-8{diff}</button>
+            <button className={increment}>+9{diff}</button>
+            <button className={decrement}>-9{diff}</button>
         </div>
     )
 }
