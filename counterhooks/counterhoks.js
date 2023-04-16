d5bbddaf19ada0bc048ca10946a51a32858b2934
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-04-16 07:01:43 +0530

    CounterHooks

--- counterhooks/counterhoks.js
+++ counterhooks/counterhoks.js
@@ -3,6 +3,9 @@ import React,{useState} from "react";
 const CounterHooks = ({diff}) => {
 
     return (
-        
+        <div>
+            <h1 className="counter-display">{count}</h1>
+            
+        </div>
     )
 }
