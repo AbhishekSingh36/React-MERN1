febcc4a26c2e773ba232970deb6e8127e6878a36
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-04-20 14:48:24 +0530

    Update Project

--- BookApp/App.js
+++ BookApp/App.js
@@ -3,6 +3,10 @@ import BookAppointment from "./session3-Takehome/BookAppointment";
 
 export default class App extends Component {
   render() {
-    
+    return (
+      <div>
+        <BookAppointment />
+      </div>
+    );
   }
 }
