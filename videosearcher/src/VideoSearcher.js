88ea57ef28da03d0f7f9493d07c7e4bbdcc7b58a
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-04-27 14:05:17 +0530

    yTube App

--- videosearcher/src/VideoSearcher.js
+++ videosearcher/src/VideoSearcher.js
@@ -3,3 +3,19 @@ import VideoCard from "./VideoCard";
 import { Grid } from "@mui/material";
 import { useEffect, useState } from "react";
 import axios from "axios";
+
+function VideoSearcher(props) {
+  // const videos = APiData.videos;
+  // console.log(videos);
+  const [videos, setVideos] = useState([]);
+  const fetchVideos = async () => {
+
+    const response = await axios.get(url);
+    const listOfVideos = response.data.video;
+    setVideos(listOfVideos);
+  };
+  
+  return
+}
+
+export default VideoSearcher;
